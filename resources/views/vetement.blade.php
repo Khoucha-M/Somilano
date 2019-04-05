bdfc8b843d7792ee9abc3461729130e6684a04db
khoucha <mook9005@gmail.com>
2019-04-05 14:13:27 +0200

    improve vetement

--- resources/views/vetement.blade.php
+++ resources/views/vetement.blade.php
@@ -34,30 +34,35 @@
 <div class="row small-up-2 medium-up-3 large-up-6">
     <div class="column">
         <img class="thumbnail" src="https://scontent.fcdg1-1.fna.fbcdn.net/v/t45.5328-0/p180x540/41986901_2250015341736434_8810708774441451520_n.jpg?_nc_cat=102&_nc_ht=scontent.fcdg1-1.fna&oh=1a3e78409f44fe3fc939e60834dba48b&oe=5D3C343C">
+        <h3>Pull  </h3>
         <h5>Pull NOELIE Pull tout doux gris</h5>
         <p>35€</p>
         <a href="#" class="button small expanded hollow">GO</a>
     </div>
     <div class="column">
         <img class="thumbnail" src="https://scontent.fcdg1-1.fna.fbcdn.net/v/t45.5328-0/p180x540/46366395_2238663149541336_8668510400205029376_n.jpg?_nc_cat=104&_nc_ht=scontent.fcdg1-1.fna&oh=d5a7eedc6b3b92c479500522a3fadeac&oe=5D4DE58E">
+        <h3>Chemisier  </h3>
         <h5>Chemisier Leo Chemisier imprimé léopard col lavallière couleurs terre, doublé </h5>
         <p>25€</p>
         <a href="#" class="button small expanded hollow">GO</a>
     </div>
     <div class="column">
         <img class="thumbnail" src="https://scontent.fcdg1-1.fna.fbcdn.net/v/t45.5328-0/p180x540/41869230_1072430886214829_4570053693541449728_n.jpg?_nc_cat=100&_nc_ht=scontent.fcdg1-1.fna&oh=736990d780706a715576ff346978b930&oe=5D4F668F">
+        <h3>Pull  </h3>
         <h5>Pull LALY pull couleur moutarde dos nu et detail sur les manches  </h5>
         <p>34€</p>
         <a href="#" class="button small expanded hollow">GO</a>
     </div>
     <div class="column">
         <img class="thumbnail" src="https://scontent.fcdg1-1.fna.fbcdn.net/v/t45.5328-0/p180x540/41792794_2178633442210034_8705233440032686080_n.jpg?_nc_cat=108&_nc_ht=scontent.fcdg1-1.fna&oh=271c81be4f5858df0cea781262e4babb&oe=5D3CAF41">
+        <h3>Top  </h3>
         <h5>Top noir dentelle doublee</h5>
         <p>17€</p>
         <a href="#" class="button small expanded hollow">GO</a>
     </div>
     <div class="column">
         <img class="thumbnail" src="https://scontent.fcdg1-1.fna.fbcdn.net/v/t45.5328-0/p180x540/46586770_1706731002765434_2019769295055093760_n.jpg?_nc_cat=109&_nc_ht=scontent.fcdg1-1.fna&oh=d4449a9e5eef35122419796fd7cc7205&oe=5D445D28">
+        <h3>Gilet  </h3>
         <h5>Gilet tout doux à boule de sapin</h5>
         <p>39€</p>
         <a href="#" class="button small expanded hollow">Buy</a>
